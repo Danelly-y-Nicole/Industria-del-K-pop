78e7afec22ec27abeceebefe57d7f6f9e50ef5ff
danelly10 <115515648+danelly10@users.noreply.github.com>
2023-08-17 14:30:40 -0400

    Update etl_module.py

--- Modulos/etl_module.py
+++ Modulos/etl_module.py
@@ -1,16 +1,40 @@
 import pandas as pd
+import numpy as np
+import plotly.express as px
+import warnings
+warnings.filterwarnings('ignore')
+from datetime import datetime
 
 def extract_data(file_path):
     data = pd.read_csv(file_path)
     return data
 
-def preprocess_data(data):
-    # Example preprocessing: selecting columns and imputing missing values
-    selected_columns = ['Height', 'Weight', 'age', 'Debut Age', 'year', 'month', 'day', 'Gender_numeric']
-    data_selected = data[selected_columns]
-    
-    imputer = SimpleImputer(strategy='mean')
-    data_imputed = imputer.fit_transform(data_selected)
-    
-    preprocessed_data = pd.DataFrame(data_imputed, columns=selected_columns)
-    return preprocessed_data
+def check_missing_ratio(data):
+    data_na = (data.isnull().sum() / len(data)) * 100
+    data_na = data_na.drop(data_na[data_na == 0].index).sort_values(ascending=False)[:30]
+    missing_data = pd.DataFrame({'Missing Ratio' :data_na})
+    return missing_data
+
+def handle_duplicates(data):
+    duplicate_rows_data = data[data.duplicated()]
+    return duplicate_rows_data
+
+def preprocess_dates(data):
+    data['Debut'] = data['Debut'].replace('0/01/1900', pd.NA)
+    data['Date of Birth'] = pd.to_datetime(data['Date of Birth'], format='%d/%m/%Y')
+    data['Debut'] = pd.to_datetime(data['Debut'], format='%d/%m/%Y')
+
+    data['age'] = (datetime.now() - data['Date of Birth']).astype('<m8[Y]')
+    data['Debut Age'] = (data['Debut'] - data['Date of Birth']).astype('<m8[Y]')
+
+    data['year'], data['month'], data['day'] = data['Date of Birth'].apply(lambda x:x.year), data['Date of Birth'].apply(lambda x:x.month), data['Date of Birth'].apply(lambda x:x.day)
+
+    return data
+
+def map_gender_to_numeric(data):
+    gender_mapping = {'M': 1, 'F': 0}
+    data['Gender_numeric'] = data['Gender'].map(gender_mapping)
+    return data
+
+def save_preprocessed_data(data, output_file):
+    data.to_csv(output_file, index=False)
