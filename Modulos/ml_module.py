032b17939971d94e327d784c4b3abba23a59d847
danelly10 <115515648+danelly10@users.noreply.github.com>
2023-08-17 14:31:50 -0400

    Update ml_module.py

--- Modulos/ml_module.py
+++ Modulos/ml_module.py
@@ -4,39 +4,59 @@ from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+import pandas as pd
 
-def train_classification_model(data):
-    X = data.drop('Gender_numeric', axis=1)
-    y = data['Gender_numeric']
-    
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    
+def prepare_data(data):
+    selected_columns = ['Height', 'Weight', 'age', 'Debut Age', 'year', 'month', 'day', 'Gender_numeric']
+    data_selected = data[selected_columns]
+    return data_selected
+
+def impute_missing_values(data):
+    imputer = SimpleImputer(strategy='mean')
+    data_imputed = imputer.fit_transform(data)
+    return data_imputed
+
+def train_logistic_regression(X_train, y_train):
     model = LogisticRegression()
     model.fit(X_train, y_train)
     return model
 
-def evaluate_classification_model(model, data):
-    X = data.drop('Gender_numeric', axis=1)
-    y = data['Gender_numeric']
-    y_pred = model.predict(X)
-    
-    accuracy = accuracy_score(y, y_pred)
+def evaluate_model(model, X_test, y_test):
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
     return accuracy
 
-def train_regression_model(data):
-    X = data.drop('Height', axis=1)
-    y = data['Height']
-    
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    
-    model = RandomForestRegressor()
-    model.fit(X_train, y_train)
-    return model
+def preprocess_data(data):
+    # Elimina columnas no relevantes
+    data = data.drop(['Stage Name', 'Full Name', 'Korean Name', 'K Stage Name', 'Gender'], axis=1)
 
-def evaluate_regression_model(model, data):
-    X = data.drop('Height', axis=1)
-    y = data['Height']
-    y_pred = model.predict(X)
-    
-    mse = mean_squared_error(y, y_pred)
+    # Codificación de variables categóricas
+    categorical_columns = ['Group', 'Company', 'Country', 'Second Country', 'Birthplace', 'Other Group', 'Former Group']
+    encoder = OneHotEncoder(drop='first', sparse=False)
+    encoded_columns = pd.DataFrame(encoder.fit_transform(data[categorical_columns]))
+    encoded_columns.columns = encoder.get_feature_names_out(categorical_columns)
+    data = pd.concat([data.drop(categorical_columns, axis=1), encoded_columns], axis=1)
+
+    # Transformación de fechas
+    data['Date of Birth'] = pd.to_datetime(data['Date of Birth'])
+    data['Debut'] = pd.to_datetime(data['Debut'])
+    reference_date = pd.to_datetime('2023-01-01')
+    data['Age_at_Debut'] = (data['Debut'] - data['Date of Birth']).dt.days
+    data = data.drop(['Date of Birth', 'Debut'], axis=1)
+
+    # Elimina filas con valores nulos
+    data = data.dropna()
+
+    return data
+
+def train_model(X_train, y_train):
+    # Entrena el modelo de RandomForestRegressor
+    regression_model = RandomForestRegressor(random_state=42)
+    regression_model.fit(X_train, y_train)
+    return regression_model
+
+def evaluate_model(model, X_test, y_test):
+    # Evalúa el modelo
+    mse = mean_squared_error(y_test, model.predict(X_test))
     return mse
